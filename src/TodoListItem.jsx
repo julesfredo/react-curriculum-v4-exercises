5dc3974d1db9e8c9f11d1f9131b17d4de1d876d3
julesfredo <salmonjulian5@gmail.com>
2026-05-06 13:48:26 -0400

    Pt2.3

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export default function TodoListItem({todo,onCompleteTodo}) {
 
 	return (
-		
-		<li>{ todo.title }</li>
-		);
+		<li>
+			<input
+				type="checkbox"
+				checked={todo.isCompleted}
+				onChange={()=> onCompleteTodo(todo.id)}
+			/>
+			{ todo.title }
+		</li>
+		)
 };
