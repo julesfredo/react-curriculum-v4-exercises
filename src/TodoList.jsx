1e7424676e11ae05db9a71cf847cd1a13c11e22a
julesfredo <salmonjulian5@gmail.com>
2026-05-06 13:23:47 -0400

    TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import TodoListItem from './TodoListItem.jsx';
 
 function ToDoList({todoList}) {
-
-	// todoList = [
-	// 	// {id: 1, title: "review resources"},
-	// 	// {id: 2, title: "take notes"},
-	// 	// {id: 3, title: "code out app"},
-	// ]
-
 	return(
 		<>
 		<ul>
